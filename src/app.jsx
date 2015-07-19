10157420c78fed161f2c6daa818cb8f5180931c2
heyheyjose <heyheyjose@users.noreply.github.com>
2015-07-18 20:25:01 -0400

    decided to go with two functions..there has to be a DRYer way

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,39 @@ var React = require('react');
 
 var Vote = React.createClass({
 
+	getInitialState: function () {
+		return {
+			awesome: 0,
+			meh: 0
+		}
+	},
+
+	awesomeCount: function () {
+		this.setState({
+			awesome: this.state.awesome + 1
+		});
+	},
+
+	mehCount: function () {
+		this.setState({
+			meh: this.state.meh + 1
+		});
+	},
+
 	render: function () {
 		return (
-			<h1>this is the voting component</h1>
+			<div>
+				<button className="btn btn-success btn-large" onClick={this.awesomeCount}>
+					Awesome!
+					<i className="glyphicon glyphicon-thumbs-up"></i>
+					( {this.state.awesome} )
+				</button>
+				<button className="btn btn-danger btn-large" onClick={this.mehCount}>
+					Meh.
+					<i className="glyphicon glyphicon-thumbs-down"></i>
+					( {this.state.meh} )
+				</button>
+			</div>
 		);
 	}
 
