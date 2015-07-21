dde85a1aa6599081dee53f26ae6b14e09ba4e1fb
heyheyjose <heyheyjose@users.noreply.github.com>
2015-07-20 23:06:14 -0400

    messed w styling

--- src/app.jsx
+++ src/app.jsx
@@ -27,12 +27,12 @@ var Vote = React.createClass({
 				<button className="btn btn-success btn-large" onClick={this.awesomeCount}>
 					Awesome!
 					<i className="glyphicon glyphicon-thumbs-up"></i>
-					( {this.state.awesome} )
+					{this.state.awesome}
 				</button>
 				<button className="btn btn-danger btn-large" onClick={this.mehCount}>
 					Meh.
 					<i className="glyphicon glyphicon-thumbs-down"></i>
-					( {this.state.meh} )
+					{this.state.meh}
 				</button>
 			</div>
 		);
